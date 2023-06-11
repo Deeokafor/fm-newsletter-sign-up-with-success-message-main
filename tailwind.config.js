64993733cf52fca40b2dadf9d100015dcb269306
deeokafor <okafor.udahdivine@gmail.com>
2023-06-11 17:08:12 +0100

    built 90% of mobile form ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.{html, js}"],
+  content: ['*.{html, js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'ns-tmt': 'hsl(4, 100%, 67%)',
+        'ns-dsg': 'hsl(234, 29%, 20%)',
+        'ns-cg': 'hsl(235, 18%, 26%)',
+        'ns-g': 'hsl(231, 7%, 60%)',
+      }
+    },
   },
   plugins: [],
   prefix: "tw-"
