d57052e9ca1573c43763b0d978006c4b9672daa7
Deeokafor <okafor.udahdivine@gmail.com>
2025-01-03 15:01:39 +0100

    Started the project all over. Some design rules were breaking the mobile screen design when rendered && completed Mobile screen design

--- assets/js/validate_form.js
+++ assets/js/validate_form.js
@@ -3,11 +3,12 @@ let validationMessage = document.getElementById('validation-message');
 let modal = document.getElementById('modal');
 let main = document.getElementById('main');
 let submittedEmailName = document.querySelector('#submitted-email-name');
-let modalDismissButton = document.querySelector('#modal-dismiss-button')
+let modalDismissButton = document.querySelector('#modal-dismiss-button');
+let footer = document.querySelector('#footer');
 
 const errorMsg = {
     err1: 'input field can\'t be empty.',
-    err2: 'Invalid format, enter a valid email.'
+    err2: /*'Invalid format, enter a valid email.'*/ 'Valid Email Required'
 }
 
 function addClass (element, className) {
@@ -18,15 +19,8 @@ function removeClass (element, className) {
     return element.classList.remove(className);
 }
 
-//returns true if input has reqqquired character
+//returns true if input has required character
 function isEmail(input) { 
-    //ditched this for a regex
-    // for (var i = 0; i < email.length; i++) {
-    //     if (email[i] === '@') {
-    //         return true;
-    //     }
-    // }
-    // return false;
     return /\S+@\S+\.\S+/.test(input);
 }
 
@@ -45,24 +39,33 @@ submitButton.addEventListener('click', function (event) {
     event.preventDefault();
 
     // Get the current value of the email input inside the event listener
-    let email = document.getElementById('email').value;
+    let email = document.getElementById('email-input');
 
     // 1. check if input field is empty
     // 2. if 1 is true, do nothing else check if input is string && email
     // 3. if 2 is true, submit email
-    if (isInputEmpty(email)) {
+    if (isInputEmpty(email.value)) {
         console.log(errorMsg.err1);
         validationMessage.textContent = errorMsg.err1;
-    } else if (isString(email) && isEmail(email)) {
-        console.log(`submitiing email:`, email);
+        addClass(email, 'tw-border-ns-tmt');
+        addClass(email, 'tw-bg-ns-tmt/10');
+    } else if (isString(email.value) && isEmail(email.value)) {
+        console.log(`submitiing email:`, email.value);
         validationMessage.textContent = null;
         addClass(main, 'tw-hidden');
         addClass(main, 'md:tw-hidden')
         removeClass(modal, 'tw-hidden');
-        submittedEmailName.textContent = email;
+        removeClass(email, 'tw-border-ns-tmt');
+        removeClass(email, 'tw-bg-ns-tmt/10');
+        removeClass(email, 'tw-text-ns-tmt');
+        addClass(footer, 'tw-hidden')
+        submittedEmailName.textContent = email.value;
     } else {
         console.log(errorMsg.err2);
         validationMessage.textContent = errorMsg.err2;
+        addClass(email, 'tw-border-ns-tmt');
+        addClass(email, 'tw-bg-ns-tmt/10');
+        addClass(email, 'tw-text-ns-tmt');
     }
 })
 
